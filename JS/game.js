f86cab5b9362655d41a779c6fbda0920a669273e
Shrika4242008 <77925895+Shrika4242008@users.noreply.github.com>
2021-07-05 11:23:51 -0400

    Add files via upload

---
+++ JS/game.js
@@ -0,0 +1,58 @@
+class Game {
+
+    constructor(){
+
+    }
+
+    getState(){
+        var stateref = database.ref("gamestate");
+        stateref.on("value",function(data){
+            gameState = data.val();
+        })
+    }
+
+    updateState(state){
+        database.ref('/').update({
+            gamestate: state
+        })
+    }
+
+    async start(){
+        if(gameState===0){
+            player = new Player();
+            var playercountref = await database.ref("playercount").once("value");
+            if (playercountref.exists){
+                pcount = playercountref.val();
+                player.getpcount();
+            }
+            form = new Form();
+            form.display();
+        }
+    }
+
+    play(){
+        form.hide();
+        textSize(10);
+        text("Game Start",120,100);
+        Player.getplayerinfo();
+        if (allplayers!=undefined){
+            display_position = 20;
+            for (var plr in allplayers){
+                if(plr==="player"+player.index){
+                    fill("red");
+                }
+                else {
+                    fill("black");
+                }
+                display_position=display_position+20;
+                text(allplayers[plr].name + ":" + allplayers[plr].distance,120,display_position);
+            }
+
+            if (keyDown(UP_ARROW)&&player.index!=null){
+                    player.distance+=50;
+                    player.updatename();
+            }
+        }
+    }
+}
+
